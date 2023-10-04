dca2c1a29454552b3c9e1d2e22ef4162422d0b81
Julia <julia.gurbanova@gmail.com>
2023-10-04 16:47:51 +0300

    initial commit

---
+++ Project19_SnowSeeker/Project19_SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project19_SnowSeekerApp.swift
+//  Project19_SnowSeeker
+//
+//  Created by Julia Gurbanova on 05.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project19_SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
